682a36bcd9ffd8f267541250a21c6bff1506c4e6
Jahanvi Rajput <jahanvirajput@Jahanvis-Laptop.local>
2024-08-13 23:50:15 +0530

    Benchmark_py

--- protes/__init__.py
+++ protes/__init__.py
@@ -6,4 +6,4 @@ from .protes_general import protes_general
 from .protes_federated_learning import protes_fed_learning
 from .protes_noisy import protes_noise
 from .protes_noise_comp import protes_noise_comparison
-
+from .Benchmark_demo_DiPTS import dipts
