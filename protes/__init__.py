baeee9cacfcd623a8f58f4d663455ee4e4884b2f
Jahanvi Rajput <jahanvirajput@Jahanvis-Laptop.local>
2024-08-14 22:58:53 +0530

    cal

--- protes/__init__.py
+++ protes/__init__.py
@@ -6,4 +6,5 @@ from .protes_general import protes_general
 from .protes_federated_learning import protes_fed_learning
 from .protes_noisy import protes_noise
 from .protes_noise_comp import protes_noise_comparison
+from .SS_PROTES import protes_subset_submod
 
